03d11ca1e1d53f0ec0f4210f300e1b0c388e1f9c
nikhilyadav566 <nikhilyad42004@gmail.com>
2026-05-29 13:47:56 +0530

    update codes

--- 02-Basic/demoshopapp/src/App.js
+++ 02-Basic/demoshopapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+// It is the App components
 function App() {
   return (
     <div className="App">
@@ -9,3 +10,15 @@ function App() {
 }
 
 export default App;
+
+
+
+// function App() {
+// Creates a function component named App.
+
+// App is a React component because:
+
+// Component names start with a capital letter (App)
+// It returns JSX (HTML-like code)
+
+// JSX (JavaScript XML) = HTML-like syntax written inside JavaScript that React converts into normal JavaScript.
--- 02-Basic/demoshopapp/src/index.js
+++ 02-Basic/demoshopapp/src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css';           // Imports global CSS styles which apply to the whole application.
+import App from './App';      // Imports the App component from App.js.
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));    // ReactDOM.createRoot(...)  it Create a React root container.
 root.render( <App />);
 
+// App is react component
+
+//     index.js is usually the entry point of a React app. 
+//   It is the first JavaScript file that runs and tells React which component to render in the browser.
+
+
+
+//         Browser opens app
+//                ↓
+//         index.html loads
+//                ↓
+//          find root div
+//                ↓
+//         index.js loads
+//                ↓
+//          load React
+//                ↓
+//          load ReactDOM
+//                ↓
+//         load index.css
+//                ↓
+//           import App
+//                ↓
+//          go to App.js
+//                ↓
+//          load App.css
+//                ↓
+//          define App
+//                ↓
+//          export App
+//                ↓
+//        back to index.js
+//                ↓
+//        find root div
+//                ↓
+//        create React root
+//                ↓
+//           render App
+//                ↓
+//          App executes
+//                ↓
+//          returns JSX
+//                ↓
+//         React converts JSX
+//                ↓
+//           updates DOM
+//                 ↓
+//             apply CSS
+//                 ↓
+//             UI visible
